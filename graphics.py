32eaad33f94da6aa412718c7209c49186d94bf7a
hemukka <henri.specops@hotmail.com>
2025-04-24 01:14:51 +0300

    draw line

--- graphics.py
+++ graphics.py
@@ -1,7 +1,23 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x:int, y:int):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1:Point, point2:Point):
+        self.start = point1
+        self.end = point2
+
+    def draw(self, canvas:Canvas, fill_color:str="black"):
+        canvas.create_line(
+            self.start.x, self.start.y, self.end.x, self.end.y,
+            fill=fill_color, width=2
+            )
+
 class Window():
-    def __init__(self, width, height):
+    def __init__(self, width:int, height:int):
         self.__root = Tk()
         self.__root.title("Maze solver")
         # self.__root.geometry(f"{width}x{height}")
@@ -24,4 +40,7 @@ class Window():
 
     def close(self):
         self.__running = False
-        
+
+    def draw_line(self, line:Line, fill_color:str="black"):
+        line.draw(self.__canvas, fill_color)
+
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+
+    # draw test lines
+    win.draw_line(Line(Point(10,10), Point(600, 500)), "red")
+    win.draw_line(Line(Point(500,30), Point(50, 550)), "black")
+    win.draw_line(Line(Point(400,30), Point(50, 50)), )
+
     win.wait_for_close()
 
 if __name__ == "__main__":
